24aed471d985b3381c33df1ad5c92c9bcec6cfc3
ayeluri <ayeluri@gmail.com>
2017-01-21 17:22:52 -0500

    artifact id updated

--- product-bundle/bundle/src/main/java/com/aem/StyleService.java
+++ product-bundle/bundle/src/main/java/com/aem/StyleService.java
@@ -7,11 +7,36 @@ import com.aem.model.StyleMaster;
 
 
 public interface StyleService {
-	
+
+    /**
+     *
+     * @param style
+     */
 	void add( StyleMaster style);
+
+    /**
+     *
+     * @return
+     */
     List<StyleMaster> getStyles() ;
+
+    /**
+     *
+     * @param id
+     * @return
+     */
     StyleMaster getStyle(int id);
+
+    /**
+     *
+     * @param styleId
+     */
     void remove (int styleId);
+
+    /**
+     *
+     * @param style
+     */
     void noStyle(StyleMaster style);
 
 }
--- product-bundle/bundle/src/main/java/com/aem/model/StyleMaster.java
+++ product-bundle/bundle/src/main/java/com/aem/model/StyleMaster.java
@@ -46,5 +46,44 @@ public class StyleMaster {
 	public void setStyleDescription(String styleDescription) {
 		this.styleDescription = styleDescription;
 	}
-	
+
+
+	@Override
+	public String toString() {
+		return "StyleMaster{" +
+				"styleId=" + styleId +
+				", styleName='" + styleName + '\'' +
+				", styleSize='" + styleSize + '\'' +
+				", styleColor='" + styleColor + '\'' +
+				", styleBrand='" + styleBrand + '\'' +
+				", styleDescription='" + styleDescription + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		StyleMaster that = (StyleMaster) o;
+
+		if (styleId != that.styleId) return false;
+		if (styleName != null ? !styleName.equals(that.styleName) : that.styleName != null) return false;
+		if (styleSize != null ? !styleSize.equals(that.styleSize) : that.styleSize != null) return false;
+		if (styleColor != null ? !styleColor.equals(that.styleColor) : that.styleColor != null) return false;
+		if (styleBrand != null ? !styleBrand.equals(that.styleBrand) : that.styleBrand != null) return false;
+		return !(styleDescription != null ? !styleDescription.equals(that.styleDescription) : that.styleDescription != null);
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = styleId;
+		result = 31 * result + (styleName != null ? styleName.hashCode() : 0);
+		result = 31 * result + (styleSize != null ? styleSize.hashCode() : 0);
+		result = 31 * result + (styleColor != null ? styleColor.hashCode() : 0);
+		result = 31 * result + (styleBrand != null ? styleBrand.hashCode() : 0);
+		result = 31 * result + (styleDescription != null ? styleDescription.hashCode() : 0);
+		return result;
+	}
 }
