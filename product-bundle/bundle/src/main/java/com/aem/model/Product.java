051ec3d7268c7cba2db7420b75252ff8622bfd5b
Shylaja <shylu2249@gmail.com>
2017-01-21 18:10:47 -0500

    added tostring,equals,hashcode

--- product-bundle/bundle/src/main/java/com/aem/model/Product.java
+++ product-bundle/bundle/src/main/java/com/aem/model/Product.java
@@ -21,7 +21,62 @@ public class Product {
         this.name = name;
     }
 
-    public String getStyle() {
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((brand == null) ? 0 : brand.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((style == null) ? 0 : style.hashCode());
+		return result;
+	}
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", description=" + description + ", id=" + id + ", style=" + style + ", brand="
+				+ brand + ", image=" + image + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (brand == null) {
+			if (other.brand != null)
+				return false;
+		} else if (!brand.equals(other.brand))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (style == null) {
+			if (other.style != null)
+				return false;
+		} else if (!style.equals(other.style))
+			return false;
+		return true;
+	}
+	public String getStyle() {
 		return style;
 	}
 
